b9c08caaf4714585fc6076c7d50a4297d8fc9ae6
TrungYU <quoctrung1220@gmail.com>
2021-01-09 15:46:15 -0800

    buttonz

--- src/js/foreground.js
+++ src/js/foreground.js
@@ -2,9 +2,15 @@
 
 console.log("from foreground");
 
-chrome.tabs.executeScript( {
-	code: "window.getSelection().toString();"
-}, function(selection) {
-	document.getElementById("textarea").value = selection[0];
-	console.log(selection[0]);
-});
+document.getElementById("analyzeBtn").addEventListener("click", analyze); 
+
+function analyze() {
+	console.log("Analyzing");
+	chrome.tabs.executeScript( {
+		code: "window.getSelection().toString();"
+	}, function(selection) {
+		document.getElementById("textarea").value = selection[0];
+		document.getElementById("infoAbtSelected").innerHTML = "Length: " + selection[0].length;
+		console.log(selection[0]);
+	});
+}
