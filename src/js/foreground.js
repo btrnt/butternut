aa3bdfbc3fb5b591d60403076ecb9525cfd76984
TrungYU <quoctrung1220@gmail.com>
2021-01-09 16:31:35 -0800

    slight changes

--- src/js/foreground.js
+++ src/js/foreground.js
@@ -4,12 +4,20 @@ console.log("from foreground");
 
 document.getElementById("analyzeBtn").addEventListener("click", analyze); 
 
+
+chrome.tabs.executeScript( {
+	code: "window.getSelection().toString();"
+}, function(selection) {
+	document.getElementById("textarea").value = selection[0];
+	console.log(selection[0]);
+});
+
+
 function analyze() {
 	console.log("Analyzing");
 	chrome.tabs.executeScript( {
 		code: "window.getSelection().toString();"
 	}, function(selection) {
-		document.getElementById("textarea").value = selection[0];
 		document.getElementById("infoAbtSelected").innerHTML = "Length: " + selection[0].length;
 		console.log(selection[0]);
 	});
