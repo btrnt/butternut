81e32b076ef2f09c0f296aaf04e5f36302e7cde1
TrungYU <quoctrung1220@gmail.com>
2021-01-10 00:26:21 -0800

    random load animation with no real effect

---
+++ src/js/coderLoader.js
@@ -0,0 +1,7 @@
+
+function startLoadingAnimation() {
+	document.getElementById('loader').innerHTML 
+	= "<div class=\"loader-container\"><div class=\"group-1\"><div class=\"line line1\"><div class=\"dash dash-1 exp-40 div\"></div><div class=\"dash dash-2 exp-70 class-name\"></div><div class=\"dash dash-3 exp-110 class\"></div><div class=\"dash dash-4 exp-70 class\"></div></div><div class=\"line line2\"><div class=\"dash dash-1 exp-40 div\"></div><div class=\"dash dash-2 exp-70 class-name\"></div><div class=\"dash dash-3 exp-90 class\"></div></div><div class=\"line line3\"><div class=\"dash dash-1 exp-300 par\"></div></div><div class=\"line line4\"><div class=\"dash dash-1 exp-200 par\"></div></div><div class=\"line line5\"><div class=\"dash dash-1 exp-40 div\"></div></div><div class=\"line line6\"><div class=\"dash dash-1 exp-40 div\"></div></div></div><div class=\"group-2\"><div class=\"line line1\"><div class=\"dash dash-1 div\"></div><div class=\"dash dash-2 class-name\"></div><div class=\"dash dash-3 class\"></div><div class=\"dash dash-4 class\"></div> </div><div class=\"line line2\"><div class=\"dash dash-1 div\"></div><div class=\"dash dash-2 exp70 class-name\"></div><div class=\"dash dash-3 exp90 class\"></div></div><div class=\"line line3\"><div class=\"dash dash-1 par\"></div></div><div class=\"line line4\"><div class=\"dash dash-1 par\"></div></div><div class=\"line line5\"><div class=\"dash dash-1 div\"></div></div><div class=\"line line6\"><div class=\"dash dash-1 div\"></div></div></div></div>";
+}
+
+// startLoadingAnimation();
