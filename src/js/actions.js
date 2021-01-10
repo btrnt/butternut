dbc105e1c00e0db1ed074c5ac7cd3bacade324a5
Vivian Wu <wu.vivian00@gmail.com>
2021-01-09 23:47:25 -0800

    resolved async call problems, calculate input text bot score

--- src/js/actions.js
+++ src/js/actions.js
@@ -33,21 +33,35 @@ async function analyze() {
 		})
 	});
 
-    var url = "http://b040ffa10081.ngrok.io/gp";
+	try{
+		let responseText = await getAnalysis();
+		let response = JSON.parse(responseText)
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", url);
+		console.log("===xhr.responseText:\n" + responseText);
+		console.log(response)
 
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		let rankLen = 50257
+		let avg = response.real_topk.reduce((total, val) => total + val[0], 0)/rankLen
+		console.log(avg)
+	}catch(e){
+		console.log(e)
+	}
+}
 
-    xhr.onreadystatechange = function () {
-       if (xhr.readyState === 4) {
-          console.log(xhr.status);
-          console.log(xhr.responseText);
-       }};
+function getAnalysis(){
+	return new Promise(function(resolve, reject){
+		var url = "http://5e42c4bac232.ngrok.io/gp";
 
-    xhr.send("text="+encodeURI(txtInput.value));
-
-	console.log("===xhr.responseText:\n" + xhr.responseText);
-
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", url);
+		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		xhr.onload = function () {
+		   if (xhr.status == 200) {
+			  	resolve(xhr.responseText)
+		   }else{
+		   		reject(xhr.status)
+		   }
+		};
+		xhr.send("text="+encodeURI(txtInput.value));
+	});
 }
